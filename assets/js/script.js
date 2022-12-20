0a91dc56500505c713f73bb2a1b50e9f417a1ad6
Vinicius <vinicius_henrique96@hotmail.com>
2022-12-20 18:51:29 -0300

    :sparkles: feat: implementing dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,29 @@
 "use strict";
 
-const navList = document.querySelector(".nav__list");
-const navImg = document.querySelectorAll(".nav__img");
-const navOptions = document.querySelectorAll(".nav__options");
+const company = document.querySelector(".nav__link-company");
+const features = document.querySelector(".nav__link-features");
+const optionsCompany = document.querySelector(".nav__options-company");
+const optionsFeatures = document.querySelector(".nav__options-box");
 
-// const navOptions = document.querySelectorAll(".nav__options");
-
-// console.log(navImg);
-// console.log(navOptions);
-
-navList.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".nav__list-item");
-
-  // Guard clause
-  if (!clicked) return;
-
-  if (clicked.classList.contains("nav__list-item")) {
-    const navImg = clicked.querySelectorAll(".nav__img");
-    const navOptions = clicked.querySelector(".nav__options");
-
-    const hiddle = function (element) {
-      const sibling = clicked[`${element}`].querySelectorAll(".nav__img");
-      sibling[0].classList.remove("active");
-      sibling[1].classList.add("active");
-
-      clicked[`${element}`]
-        .querySelector(".nav__options")
-        .classList.add("hidden");
-    };
-
-    navImg.forEach((img) => {
-      if (img.classList.contains("active")) {
-        img.classList.remove("active");
-        navOptions.classList.remove("hidden");
-
-        clicked.children[0].innerText === "Features"
-          ? hiddle("nextElementSibling")
-          : hiddle("previousElementSibling");
-      } else {
-        img.classList.add("active");
-        navOptions.classList.add("hidden");
-      }
-    });
+const upDownCompany = function (e) {
+  e.preventDefault();
+  company.classList.toggle("active");
+  optionsCompany.classList.toggle("active");
+  if (features.classList.contains("active")) {
+    features.classList.remove("active");
+    optionsFeatures.classList.remove("active");
   }
-});
+};
+
+const upDownFeatures = function (e) {
+  e.preventDefault();
+  features.classList.toggle("active");
+  optionsFeatures.classList.toggle("active");
+  if (company.classList.contains("active")) {
+    company.classList.remove("active");
+    optionsCompany.classList.remove("active");
+  }
+};
+
+company.addEventListener("click", upDownCompany);
+features.addEventListener("click", upDownFeatures);
