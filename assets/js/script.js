d831ff5aa87070a393333c77f98cab37a2164320
Vinicius <vinicius_henrique96@hotmail.com>
2022-12-24 11:49:24 -0300

    :sparkles: implementing hamburger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,12 @@ const company = document.querySelector(".nav__link-company");
 const features = document.querySelector(".nav__link-features");
 const optionsCompany = document.querySelector(".nav__options-company");
 const optionsFeatures = document.querySelector(".nav__options-box");
+const nav = document.querySelector(".nav");
+const menuBtn = document.querySelector(".menu");
+const closeBtn = document.querySelector(".close");
+const overlay = document.querySelector(".overlay");
+const eventAdd = "click";
+const classAct = "active";
 
 const upDownCompany = function (e) {
   e.preventDefault();
@@ -13,6 +19,11 @@ const upDownCompany = function (e) {
     features.classList.remove("active");
     optionsFeatures.classList.remove("active");
   }
+
+  outsideClick(company, eventAdd, () => {
+    company.classList.remove(classAct);
+    optionsCompany.classList.remove(classAct);
+  });
 };
 
 const upDownFeatures = function (e) {
@@ -23,7 +34,51 @@ const upDownFeatures = function (e) {
     company.classList.remove("active");
     optionsCompany.classList.remove("active");
   }
+
+  outsideClick(features, eventAdd, () => {
+    features.classList.remove(classAct);
+    optionsFeatures.classList.remove(classAct);
+  });
 };
 
 company.addEventListener("click", upDownCompany);
 features.addEventListener("click", upDownFeatures);
+
+menuBtn.addEventListener("click", function () {
+  nav.classList.add("active");
+  menuBtn.classList.add("active");
+  closeBtn.classList.add("active");
+  overlay.classList.add("active");
+
+  outsideClick(nav, eventAdd, () => {
+    nav.classList.remove("active");
+    menuBtn.classList.remove("active");
+    closeBtn.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+closeBtn.addEventListener("click", function () {
+  nav.classList.remove("active");
+  menuBtn.classList.remove("active");
+  closeBtn.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+function outsideClick(element, eventAdd, callback) {
+  const html = document.querySelector("html");
+  const outside = "data-outside";
+
+  if (!element.hasAttribute(outside)) {
+    setTimeout(() => html.addEventListener(eventAdd, handleOutsideClick));
+    element.setAttribute(outside, "");
+  }
+
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      html.removeEventListener(eventAdd, handleOutsideClick);
+      callback();
+    }
+  }
+}
