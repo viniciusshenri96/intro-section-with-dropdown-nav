0b1a43bcdacb654ab935cb859d7e15fdc1bac1b7
Vinicius <vinicius_henrique96@hotmail.com>
2022-12-30 18:32:14 -0300

    :sparkles: feat: implementing mobile menu

---
+++ assets/js/modules/outsideClick.js
@@ -0,0 +1,23 @@
+export default function clickOutside(element, events, callback) {
+  const html = document.querySelector("html");
+  const outside = "data-outside";
+
+  // Só vai acontecer por causa do efeito bubble
+  if (!element.hasAttribute(outside)) {
+    setTimeout(() => {
+      html.addEventListener(events, handleOutsideClick);
+    });
+    element.setAttribute(outside, "");
+  }
+  // element.setAttribute(outside, '')
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      setTimeout(() => {
+        // Removendo o evento de click do html, para ele ocorrer apenas uma vez
+        html.removeEventListener(events, handleOutsideClick);
+      });
+      callback();
+    }
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,64 @@
-"use strict";
+import outside from "./modules/outsideClick.js";
 
-const dropDownMenus = document.querySelectorAll("[data-dropdown]");
+// Selecting element for dropdown menu
+const dropDownMenu = document.querySelectorAll("[data-dropdown]");
 const feacturesUp = document.querySelector(".features__up");
 const companyUp = document.querySelector(".company__up");
 const feacturesDown = document.querySelector(".features__down");
 const companyDown = document.querySelector(".company__down");
-console.log(dropDownMenus);
+
+// Selecting element for mobine
+const headerList = document.querySelector(".header__list");
+const headerBtn = document.querySelector(".header__btn");
+const headerMenu = document.querySelector(".header__menu");
+const menuClose = document.querySelector(".menu-close");
+const overlay = document.querySelector(".overlay");
+
+// Dropdown menu
+const classActive = "active";
+const classListPro = function (pro) {
+  feacturesUp.classList[pro](classActive);
+  companyUp.classList[pro](classActive);
+  feacturesDown.classList[pro](classActive);
+  companyDown.classList[pro](classActive);
+};
 
 const handleClick = function (e) {
   e.preventDefault();
-  this.classList.add("active");
-  feacturesUp.classList.add("active");
-  companyUp.classList.add("active");
-  feacturesDown.classList.add("active");
-  companyDown.classList.add("active");
+  this.classList.toggle(classActive);
+  feacturesUp.classList.toggle(classActive);
+  companyUp.classList.toggle(classActive);
+  feacturesDown.classList.toggle(classActive);
+  companyDown.classList.toggle(classActive);
 
-  outsideClick(this, ["touchstart", "click"], () => {
-    this.classList.remove("active");
-    feacturesUp.classList.remove("active");
-    companyUp.classList.remove("active");
-    feacturesDown.classList.remove("active");
-    companyDown.classList.remove("active");
+  outside(this, "click", () => {
+    this.classList.remove(classActive);
+    feacturesUp.classList.remove(classActive);
+    companyUp.classList.remove(classActive);
+    feacturesDown.classList.remove(classActive);
+    companyDown.classList.remove(classActive);
   });
 };
 
-dropDownMenus.forEach((menu) => {
-  ["touchstart", "click"].forEach((userEvent) => {
-    menu.addEventListener(userEvent, handleClick);
-  });
+dropDownMenu.forEach((menu) => {
+  menu.addEventListener("click", handleClick);
 });
 
-function outsideClick(element, events, callback) {
-  const html = document.documentElement;
-  const outside = "data-outside";
+// Menu Mobile
+const openMenu = function () {
+  headerList.classList.toggle("active");
+  headerBtn.classList.toggle("active");
+  headerMenu.classList.toggle("active");
+  menuClose.classList.toggle("active");
+  overlay.classList.toggle("active");
 
-  // Só vai acontecer por causa do efeito bubble
-  if (!element.hasAttribute(outside)) {
-    events.forEach((userEvent) => {
-      html.addEventListener(userEvent, handleOutsideClick);
-    });
-    element.setAttribute(outside, "");
-  }
-  // element.setAttribute(outside, '')
-  function handleOutsideClick(event) {
-    if (!element.contains(event.target)) {
-      element.removeAttribute(outside);
-      events.forEach((userEvent) => {
-        // Removendo o evento de click do html, para ele ocorrer apenas uma vez
-        html.removeEventListener(userEvent, handleOutsideClick);
-      });
-      callback();
-    }
-  }
-}
+  outside(headerList, "click", () => {
+    headerList.classList.remove("active");
+    headerBtn.classList.remove("active");
+    headerMenu.classList.remove("active");
+    menuClose.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+};
+
+headerMenu.addEventListener("click", openMenu);
