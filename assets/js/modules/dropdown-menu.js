3c3e40a73c099531854c4822885f799e5093796a
Vinicius <vinicius_henrique96@hotmail.com>
2023-01-05 16:59:59 -0300

    :ok_hand: Code review changes

--- assets/js/modules/dropdown-menu.js
+++ assets/js/modules/dropdown-menu.js
@@ -1,42 +1,45 @@
-export default function showDropdown(e) {
-  if (e.type === "touchstart") {
-    document.removeEventListener("click", showDropdown);
-  }
-  console.log(e);
-  let dropdownButton;
-  let currentDropdown;
-  const classActive = "active";
-  dropdownButton = e.target.matches("[data-dropdown-button]");
-  dropdownButton = e.target.closest("[data-dropdown-button]");
-  currentDropdown = e.target.closest("[data-dropdown]");
-
-  if (!dropdownButton && e.target.closest("[data-dropdown]")) return;
-
-  if (dropdownButton) {
-    currentDropdown.classList.toggle(classActive);
-    dropdownButton.classList.toggle(classActive);
-    if (!currentDropdown.classList.contains(classActive)) {
-      return currentDropdown.setAttribute("aria-expanded", "false");
+export default function dropDownMenuFunction() {
+  function showDropdown(e) {
+    if (e.type === "touchstart") {
+      document.removeEventListener("click", showDropdown);
     }
-    currentDropdown.setAttribute("aria-expanded", "true");
+    let dropdownButton;
+    let currentDropdown;
+    const classActive = "active";
+    dropdownButton = e.target.matches("[data-dropdown-button]");
+    dropdownButton = e.target.closest("[data-dropdown-button]");
+    currentDropdown = e.target.closest("[data-dropdown]");
+
+    if (!dropdownButton && e.target.closest("[data-dropdown]")) return;
+
+    if (dropdownButton) {
+      currentDropdown.classList.toggle(classActive);
+      dropdownButton.classList.toggle(classActive);
+      if (!currentDropdown.classList.contains(classActive)) {
+        return currentDropdown.setAttribute("aria-expanded", "false");
+      }
+      currentDropdown.setAttribute("aria-expanded", "true");
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+      if (dropdown === currentDropdown) return;
+      dropdown.classList.remove(classActive);
+    });
+
+    document
+      .querySelectorAll("[data-dropdown-button].active")
+      .forEach((menu) => {
+        if (dropdownButton === menu) return;
+        menu.classList.remove(classActive);
+      });
   }
 
-  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
-    if (dropdown === currentDropdown) return;
-    dropdown.classList.remove(classActive);
+  window.addEventListener("resize", function () {
+    if (window.matchMedia("min-widh: 1400px")) {
+      document.addEventListener("click", showDropdown);
+    }
   });
 
-  document.querySelectorAll("[data-dropdown-button].active").forEach((menu) => {
-    if (dropdownButton === menu) return;
-    menu.classList.remove(classActive);
-  });
+  document.addEventListener("touchstart", showDropdown);
+  document.addEventListener("click", showDropdown);
 }
-
-window.addEventListener("resize", function () {
-  if (window.matchMedia("min-widh: 1400px")) {
-    document.addEventListener("click", showDropdown);
-  }
-});
-
-document.addEventListener("touchstart", showDropdown);
-document.addEventListener("click", showDropdown);
--- assets/js/modules/menu-mobile.js
+++ assets/js/modules/menu-mobile.js
@@ -1,28 +1,29 @@
 import outside from "./outsideClick.js";
 
-const headerList = document.querySelector("[data-menu-list]");
-const headerBtn = document.querySelector("[data-menu-btn]");
-const mobileBtn = document.querySelector("[data-menu-button]");
-const menuClose = document.querySelector("[data-menu-close]");
-const overlay = document.querySelector("[data-menu-overlay]");
-const classActive = "active";
+export default function menuMobileFunction() {
+  const headerList = document.querySelector("[data-menu-list]");
+  const headerBtn = document.querySelector("[data-menu-btn]");
+  const mobileBtn = document.querySelector("[data-menu-button]");
+  const menuClose = document.querySelector("[data-menu-close]");
+  const overlay = document.querySelector("[data-menu-overlay]");
+  const classActive = "active";
 
-const classListProMenuMobile = function (pro) {
-  const elements = [headerList, headerBtn, mobileBtn, menuClose, overlay];
-  elements.forEach((el) => el.classList[pro](classActive));
-};
+  const classListProMenuMobile = function (pro) {
+    const elements = [headerList, headerBtn, mobileBtn, menuClose, overlay];
+    elements.forEach((el) => el.classList[pro](classActive));
+  };
 
-export default function openMenu() {
-  classListProMenuMobile("toggle");
-  console.log();
-  if (!this.classList.contains(classActive))
-    return this.setAttribute("aria-expanded", "false");
+  function openMenu() {
+    classListProMenuMobile("toggle");
+    if (!this.classList.contains(classActive))
+      return this.setAttribute("aria-expanded", "false");
 
-  this.setAttribute("aria-expanded", "true");
+    this.setAttribute("aria-expanded", "true");
 
-  outside(headerList, () => {
-    classListProMenuMobile("remove");
-  });
+    outside(headerList, () => {
+      classListProMenuMobile("remove");
+    });
+  }
+
+  mobileBtn.addEventListener("click", openMenu);
 }
-
-mobileBtn.addEventListener("click", openMenu);
